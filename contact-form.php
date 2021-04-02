56862b1722851545b01af3cdacd5a345873971ac
WeekieNHN <frankpodraza@icloud.com>
2021-04-02 16:58:07 -0700

    push to form text

---
+++ contact-form.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $mailfrom = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  $mailto = "nerdherdnetwork@gmail.com";
+  $headers = "From: ".$mailfrom;
+  $txt = "This is an email from ".$name." on www.nerdherd.network.\n\n".$message;
+
+  mail($mailto, $subject, $txt, $headers);
+
+  header("Location: index.php?mailsend");
+
+}
